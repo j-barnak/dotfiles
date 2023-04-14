61a3609e89768d3b1987c27d336d82f83f469fc0
Jared B <jared.j.barnak@gmail.com>
2023-04-14 03:01:14 -0400

    updated

--- neovim/.config/nvim/lua/config/remap.lua
+++ neovim/.config/nvim/lua/config/remap.lua
@@ -1,7 +1,8 @@
 vim.g.mapleader = " "
-vim.api.nvim_set_keymap('i', 'jj', '<Esc>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>l", vim.lsp.buf.format)
