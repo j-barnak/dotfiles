3bfc9af991d70a6c6823d31eba9e8243ea85d7d9
Jared B <jared.j.barnak@gmail.com>
2023-04-15 15:56:45 -0400

    nvim updated

--- neovim/.config/nvim/lua/plugins/aerial.lua
+++ neovim/.config/nvim/lua/plugins/aerial.lua
@@ -1,7 +1,6 @@
+require('telescope').load_extension('aerial')
 require('aerial').setup({
     close_on_select = true,
 })
-
-require('telescope').load_extension('aerial')
 vim.keymap.set('n', '<leader>d', '<cmd>AerialToggle<CR>')
 vim.keymap.set('n', '<leader>a', '<cmd>Telescope aerial<CR>')
