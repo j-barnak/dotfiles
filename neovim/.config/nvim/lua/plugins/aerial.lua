c86fd78466ad7838f5d30f2c02037df527de5af2
Jared B <jared.j.barnak@gmail.com>
2023-04-15 16:26:00 -0400

    Updated width

--- neovim/.config/nvim/lua/plugins/aerial.lua
+++ neovim/.config/nvim/lua/plugins/aerial.lua
@@ -1,6 +1,12 @@
 require('telescope').load_extension('aerial')
 require('aerial').setup({
+    layout = {
+        max_width = { 40, 0.24 },
+        width = nil,
+        min_width = 10,
+    },
     close_on_select = true,
 })
+
 vim.keymap.set('n', '<leader>d', '<cmd>AerialToggle<CR>')
 vim.keymap.set('n', '<leader>a', '<cmd>Telescope aerial<CR>')
