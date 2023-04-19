56a3808a9b848e04f153776838092e10c23583f3
Jared B <jared.j.barnak@gmail.com>
2023-04-19 01:02:54 -0400

    nvim

--- neovim/.config/nvim/lua/config/colorscheme.lua
+++ neovim/.config/nvim/lua/config/colorscheme.lua
@@ -1,17 +1,16 @@
--- -- Colorschme: Sonokai
--- -- https://github.com/sainnhe/sonokai
--- vim.g.sonokai_style = 'andromeda'
--- vim.g.sonokai_better_performance = 1
--- vim.cmd('colorscheme sonokai')
+-- Colorschme: Sonokai
+-- https://github.com/sainnhe/sonokai
+vim.g.sonokai_style = 'default'
+vim.g.sonokai_better_performance = 1
+vim.cmd('colorscheme sonokai')
 
-
--- Colorschme: Catppuccin
--- https://github.com/catppuccin/nvim
-require("catppuccin").setup({
-    flavour = "frappe",
-    no_italic = true
-})
-vim.cmd.colorscheme "catppuccin-frappe"
+-- -- Colorschme: Catppuccin
+-- -- https://github.com/catppuccin/nvim
+-- require("catppuccin").setup({
+--     flavour = "frappe",
+--     no_italic = true
+-- })
+-- vim.cmd.colorscheme "catppuccin-frappe"
 
 -- -- Colorschme: One Dark
 -- -- https://github.com/joshdick/onedark.vim
