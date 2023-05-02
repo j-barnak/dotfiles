161d7c18fe4766d0c8411fa47846bf0de01eca7f
Jared B <jared.j.barnak@gmail.com>
2023-05-02 15:50:25 -0400

    Toward Migration

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,16 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    lazy = false,
-    tag = '0.1.1',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>ps',
-            function()
-                builtin.grep_string({ search = vim.fn.input(" Grep > ") })
-            end)
-    end
+	'nvim-telescope/telescope.nvim',
+	lazy = false,
+	tag = '0.1.1',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
+		vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+		vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+		vim.keymap.set('n', '<leader>ps',
+			function()
+				builtin.grep_string({ search = vim.fn.input(" Grep > ") })
+			end)
+	end
 }
--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,29 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = function()
-        require('nvim-treesitter.install').update({ with_sync = true })
-    end,
-    lazy = false,
-    opts = {
-        -- A list of parser names, or 'all' (the five listed parsers should always be installed)
-        ensure_installed = { 'c', 'lua', 'javascript', 'cpp', 'typescript', 'vim', 'html', 'css', 'bash', 'vimdoc',
-            'query' },
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-        highlight = {
-            enable = true,
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
-        },
-    },
-    config = function()
-        require('nvim-treesitter.configs').setup(opts)
-    end,
+	'nvim-treesitter/nvim-treesitter',
+	build = function()
+		require('nvim-treesitter.install').update({ with_sync = true })
+	end,
+	lazy = false,
+	config = function()
+		local options = {
+			-- A list of parser names, or 'all' (the five listed parsers should always be installed)
+			ensure_installed = { 'c', 'lua', 'javascript', 'cpp', 'typescript', 'vim', 'html', 'css',
+				'bash', 'vimdoc',
+				'query' },
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
+			-- Automatically install missing parsers when entering buffer
+			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			auto_install = true,
+			highlight = {
+				enable = true,
+				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+				-- Using this option may slow down your editor, and you may see some duplicate highlights.
+				-- Instead of true it can also be a list of languages
+				additional_vim_regex_highlighting = false,
+			},
+		}
+		require('nvim-treesitter.configs').setup(options)
+	end,
 }
