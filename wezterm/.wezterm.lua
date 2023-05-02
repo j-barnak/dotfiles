548884d7c408a063eed821b3b8509c3a339db74f
Jared B <jared.j.barnak@gmail.com>
2023-05-02 19:33:18 -0400

    Updated

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,7 +9,7 @@ end
 config.hide_tab_bar_if_only_one_tab = true
 
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.font = wezterm.font("Berkeley Mono", { weight = "Regular", stretch = "Normal", style = "Normal" })
 
 config.color_scheme = 'Monokai Pro (Gogh)'
 
