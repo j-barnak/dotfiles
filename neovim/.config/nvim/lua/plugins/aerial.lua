1d07732e6ee946123d3a96516e745652c9fe3a6c
Jared B <jared.j.barnak@gmail.com>
2023-05-03 12:24:54 -0400

    Fixed aerial and added telescope

--- neovim/.config/nvim/lua/plugins/aerial.lua
+++ neovim/.config/nvim/lua/plugins/aerial.lua
@@ -6,10 +6,6 @@ return {
         'nvim-tree/nvim-web-devicons',
     },
     keys = {
-        { '<leader>dd', '<cmd>AerialToggle<cr>',     desc = 'List symbols' },
-        { '<leader>df', '<cmd>Telescope aerial<cr>', desc = 'Search symbols' },
-    },
-    config = function()
-        require('telescope').load_extension('aerial')
-    end,
+        { '<leader>dd', '<cmd>AerialToggle<cr>', desc = 'List symbols' },
+    }
 }
--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,34 +1,51 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim',
-        dependencies = {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
             'nvim-telescope/telescope-fzf-native.nvim',
             build = 'make',
-            config = function()
-                require('telescope').load_extension('fzf')
-            end,
         },
-        keys = {
-            {
-                '<leader>ff',
-                '<cmd>Telescope find_files<CR>',
-                desc = 'Find Files'
-            },
-            {
-                '<leader>g',
-                '<cmd> Telescope live_grep<CR>',
-                desc = 'Grep all files',
-            },
-            {
-                '<leader>fr',
-                '<cmd>Telescope oldfiles<cr>',
-                desc = 'Find recent files',
-            },
-            {
-                '<leader>fp',
-                '<cmd>Telescope git_files<cr>',
-                desc = 'Find git files',
-            },
+        'debugloop/telescope-undo.nvim',
+        'stevearc/aerial.nvim'
+    },
+    keys = {
+        {
+            '<leader>ff',
+            '<cmd>Telescope find_files<CR>',
+            desc = 'Find Files'
+        },
+        {
+            '<leader>fg',
+            '<cmd> Telescope live_grep<CR>',
+            desc = 'Grep all files',
+        },
+        {
+            '<leader>fr',
+            '<cmd>Telescope oldfiles<cr>',
+            desc = 'Find recent files',
+        },
+        {
+            '<leader>fp',
+            '<cmd>Telescope git_files<cr>',
+            desc = 'Find project files',
+        },
+        {
+            '<leader>fs',
+            '<cmd>Telescope aerial<cr>',
+            desc = 'Find symbols',
+        },
+        {
+            '<leader>u',
+            '<cmd>Telescope undo<cr>',
+            desc = 'Undo',
         },
     },
+    config = function()
+        -- Load the extensions
+        require('telescope').load_extension('fzf')
+        require('telescope').load_extension('undo')
+        require('telescope').load_extension('aerial')
+    end,
+
 }
