9d4b3e012cd479dec2c1d8521574f8e098709c24
Jared B <jared.j.barnak@gmail.com>
2023-05-05 14:54:03 -0400

    mini.starter config almost done

--- neovim/.config/nvim/lua/plugins/starter.lua
+++ neovim/.config/nvim/lua/plugins/starter.lua
@@ -13,8 +13,13 @@ return {
     }, "\n"),
     footer = os.date(),
     items = {
-      { name = "Open Projects", action = "Telescope projects", section = "Telescope" },
+      { name = "Open Projects",        action = "Telescope projects",   section = "Actions" },
+      { name = "Search project files", action = "Telescope git_files",  section = "Actions" },
+      { name = "Find files",           action = "Telescope find_files", section = "Actions" },
+      { name = "View recent files",    action = "Telescope oldfiles",   section = "Actions" },
+      { name = "Planets!",             action = "Telescope planets",    section = "Actions" },
     },
+
   },
   config = function(_, opts)
     require("mini.starter").setup(opts)
