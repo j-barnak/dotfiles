5ac42d1c14921aa13f61387ccc24d2c912ff26ce
Jared B <jared.j.barnak@gmail.com>
2023-05-07 19:17:06 -0400

    New

--- neovim/.config/nvim/lua/plugins/comment.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "numToStr/Comment.nvim",
-  opts = {
-    toggler = { line = "<leader>cc", block = "<leader>bc" },
-    opleader = { line = "<leader>c", block = "<leader>b" },
-    extra = { above = "<leader>cO", below = "<leader>co", eol = "<leader>cA" },
-  },
-}
