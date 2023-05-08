f5edfb2a6b0b0c1cc05df664aa4c227449d9ffd7
Jared B <jared.j.barnak@gmail.com>
2023-05-07 22:31:36 -0400

    nvim: registered events in lsp-zerp

--- neovim/.config/nvim/lua/plugins/lsp-zero.lua
+++ neovim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -86,6 +86,23 @@ return {
             })
 
             lsp.setup()
+
+            -- Added registered events @ begin
+
+            lsp.preset('recommended')
+            local cmp = require('cmp')
+
+            vim.g.testing_cmp_event = 'NOP'
+
+            cmp.event:on('confirm_done', function(evt)
+                if evt.entry then
+                    vim.g.testing_cmp_event = 'IT WORKS'
+                else
+                    vim.g.testing_cmp_event = 'Yes. Everything is good'
+                end
+            end)
         end,
+
+        -- @ end
     },
 }
