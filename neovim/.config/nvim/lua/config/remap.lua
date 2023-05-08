4800661c9616d67b77b6f87697bf96af9ef70f74
Jared B <jared.j.barnak@gmail.com>
2023-05-07 20:14:52 -0400

    Nvim: Working State

---
+++ neovim/.config/nvim/lua/config/remap.lua
@@ -0,0 +1,5 @@
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
---
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/neodev.nvim",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    build = "make install_jsregexp",
+  },
+  { "tpope/vim-fugitive" },
+  {
+    "tpope/vim-repeat",
+    event = "VeryLazy",
+  },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+  },
+}
