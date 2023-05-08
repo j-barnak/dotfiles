20f8a5dd3f43a082549257dd91d2eba941e09df0
Jared B <jared.j.barnak@gmail.com>
2023-05-07 23:23:59 -0400

    Toward fixing autopair --> edited lsp-zero

--- neovim/.config/nvim/lua/plugins/lsp-zero.lua
+++ neovim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -9,14 +9,6 @@ return {
             -- Note: autocompletion settings will not take effect
 
             require("lsp-zero.settings").preset({})
-            -- Added the following @ begin
-            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-            local cmp = require('cmp')
-            cmp.event:on(
-                'confirm_done',
-                cmp_autopairs.on_confirm_done()
-            )
-            -- @ end
         end,
     },
 
@@ -94,6 +86,49 @@ return {
             })
 
             lsp.setup()
+
+            -- Added the following @ begin
+            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+            local cmp = require('cmp')
+            --
+            local handlers = require('nvim-autopairs.completion.handlers')
+
+            cmp.event:on(
+                'confirm_done',
+                cmp_autopairs.on_confirm_done({
+                    filetypes = {
+                        -- "*" is a alias to all filetypes
+                        ["*"] = {
+                            ["("] = {
+                                kind = {
+                                    cmp.lsp.CompletionItemKind.Function,
+                                    cmp.lsp.CompletionItemKind.Method,
+                                },
+                                handler = handlers["*"]
+                            }
+                        },
+                        lua = {
+                            ["("] = {
+                                kind = {
+                                    cmp.lsp.CompletionItemKind.Function,
+                                    cmp.lsp.CompletionItemKind.Method
+                                },
+                                ---@param char string
+                                ---@param item table item completion
+                                ---@param bufnr number buffer number
+                                ---@param rules table
+                                ---@param commit_character table<string>
+                                handler = function(char, item, bufnr, rules, commit_character)
+                                    -- Your handler function. Inpect with print(vim.inspect{char, item, bufnr, rules, commit_character})
+                                end
+                            }
+                        },
+                        -- Disable for tex
+                        tex = false
+                    }
+                })
+            )
+            -- @ end
         end,
     },
 }
