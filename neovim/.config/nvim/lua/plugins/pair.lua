40dbe0a135bef89318b4233cbdde50d5f6441b6a
Jared B <jared.j.barnak@gmail.com>
2023-05-08 07:28:56 -0400

    Setting the keyboard protocol to kitty broke everything so I removed it

--- neovim/.config/nvim/lua/plugins/pair.lua
+++ neovim/.config/nvim/lua/plugins/pair.lua
@@ -1,13 +1,7 @@
 return {
-    "windwp/nvim-autopairs",
-    -- Attempt at autopair | delete this @ begin
-    config = function()
-        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-        local cmp = require('cmp')
-        cmp.event:on(
-            'confirm_done',
-            cmp_autopairs.on_confirm_done()
-        )
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    config = function(_, opts)
+        require("mini.pairs").setup(opts)
     end,
-    -- @ end
 }
