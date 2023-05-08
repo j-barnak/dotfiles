0dce255ffd2d3f5ee92ca24da6e4c9695ef0c254
Jared B <jared.j.barnak@gmail.com>
2023-05-08 00:12:40 -0400

    Another failure to fix bug

--- neovim/.config/nvim/lua/plugins/pair.lua
+++ neovim/.config/nvim/lua/plugins/pair.lua
@@ -1,5 +1,13 @@
 return {
     "windwp/nvim-autopairs",
-    event = "VeryLazy",
-    opts = {}
+    -- Attempt at autopair | delete this @ begin
+    config = function()
+        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+        local cmp = require('cmp')
+        cmp.event:on(
+            'confirm_done',
+            cmp_autopairs.on_confirm_done()
+        )
+    end,
+    -- @ end
 }
