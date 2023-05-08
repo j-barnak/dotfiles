fe1b411a2e5274935966644a83dcddf3c140d863
Jared B <jared.j.barnak@gmail.com>
2023-05-08 17:30:05 -0400

    nvim: simplified nvim-autopairs

--- neovim/.config/nvim/lua/plugins/pair.lua
+++ neovim/.config/nvim/lua/plugins/pair.lua
@@ -5,44 +5,19 @@ return {
   },
   event = "VeryLazy",
   config = function()
-    --
-    local handlers = require('nvim-autopairs.completion.handlers')
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     local cmp = require('cmp')
     cmp.event:on(
       'confirm_done',
-      cmp_autopairs.on_confirm_done({
-        filetypes = {
-          -- "*" is a alias to all filetypes
-          ["*"] = {
-            ["("] = {
-              kind = {
-                cmp.lsp.CompletionItemKind.Function,
-                cmp.lsp.CompletionItemKind.Method,
-              },
-              handler = handlers["*"]
-            }
-          },
-          lua = {
-            ["("] = {
-              kind = {
-                cmp.lsp.CompletionItemKind.Function,
-                cmp.lsp.CompletionItemKind.Method
-              },
-              ---@param char string
-              ---@param item table item completion
-              ---@param bufnr number buffer number
-              ---@param rules table
-              ---@param commit_character table<string>
-              handler = function(char, item, bufnr, rules, commit_character)
-                -- Your handler function. Inpect with print(vim.inspect{char, item, bufnr, rules, commit_character})
-              end
-            }
-          },
-        }
-      })
+      cmp_autopairs.on_confirm_done()
     )
-    --
     require("nvim-autopairs").setup {}
   end
 }
+-- return {
+--   "echasnovski/mini.pairs",
+--   event = "VeryLazy",
+--   config = function(_, opts)
+--     require("mini.pairs").setup(opts)
+--   end,
+-- }
