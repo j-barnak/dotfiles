623ace69a5da8ce3eb6a8be22d630e0d6192cd92
Jared B <jared.j.barnak@gmail.com>
2023-05-10 20:31:02 -0400

    Adding lazygit to replace fugitive

--- neovim/.config/nvim/lua/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "tpope/vim-fugitive",
-  event = "VeryLazy",
-  keys = {
-    { "<leader>gg", "<cmd>tab Git commit<cr>", desc = "Git commit in separate tab" },
-    { "<leader>gP", "<cmd>Git push<cr>",       desc = "Push commit to remote" }
-  }
-}
---
+++ neovim/.config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,7 @@
+return {
+  "kdheepak/lazygit.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Git commit in separate tab" },
+  }
+}
