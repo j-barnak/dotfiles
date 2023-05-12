78c99834b11f51dd5c24405248a708c466b4a322
Jared B <jared.j.barnak@gmail.com>
2023-05-11 23:22:35 -0400

    Added `null-ls` to `lsp-zero` to replace `lsp-formatter`
    It's almost set up, I just need to to figure out why
    `stylua` is acting up.

--- neovim/.config/nvim/lua/plugins/lsp-zero.lua
+++ neovim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -1,71 +1,71 @@
 return {
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    lazy = false,
-    config = function()
-      -- This is where you modify the settings for lsp-zero
-      -- Note: autocompletion settings will not take effect
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		lazy = false,
+		config = function()
+			-- This is where you modify the settings for lsp-zero
+			-- Note: autocompletion settings will not take effect
 
-      require("lsp-zero.settings").preset({})
-    end,
-  },
+			require("lsp-zero.settings").preset({})
+		end,
+	},
 
-  -- Autocompletion
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      { "L3MON4D3/LuaSnip" },
-    },
-    config = function()
-      require("lsp-zero.cmp").extend()
+	-- Autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{ "L3MON4D3/LuaSnip" },
+		},
+		config = function()
+			require("lsp-zero.cmp").extend()
 
-      -- And you can configure cmp even more, if you want to.
-      local cmp = require("cmp")
-      local cmp_action = require("lsp-zero.cmp").action()
+			-- And you can configure cmp even more, if you want to.
+			local cmp = require("cmp")
+			local cmp_action = require("lsp-zero.cmp").action()
 
-      cmp.setup({
-        mapping = {
-          ["<Tab>"] = cmp.mapping.select_next_item(select_opts),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(select_opts),
-          ["<cr>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }),
-        },
-      })
-    end,
-  },
+			cmp.setup({
+				mapping = {
+					["<Tab>"] = cmp.mapping.select_next_item(select_opts),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(select_opts),
+					["<cr>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					}),
+				},
+			})
+		end,
+	},
 
-  -- LSP
-  {
-    "neovim/nvim-lspconfig",
-    cmd = "LspInfo",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      { "lukas-reineke/lsp-format.nvim" },
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "williamboman/mason-lspconfig.nvim" },
-      {
-        "williamboman/mason.nvim",
-        build = function()
-          pcall(vim.cmd, "MasonUpdate")
-        end,
-      },
-    },
-    --  TODO: When lsp.format_on_save becomes non-experimental, change lsp-format to lsp.format_on_save
-    --        https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/api-reference.md#format_on_saveopts
-    config = function()
-      local lsp = require("lsp-zero")
-      local auto_format = function(_client) require("lsp-format").on_attach(_client) end
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		cmd = "LspInfo",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "jose-elias-alvarez/null-ls.nvim" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{
+				"williamboman/mason.nvim",
+				build = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+		},
+		--  TODO: When lsp.format_on_save becomes non-experimental, change lsp-format to lsp.format_on_save
+		--        https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/api-reference.md#format_on_saveopts
+
+		--  TODO: To get the formatter to properly work, I believe I need null-ls or lsp-formatter
+		config = function()
+			local lsp = require("lsp-zero")
 
       -- stylua: ignore start
       lsp.on_attach(function(client, bufnr)
         lsp.default_keymaps({ buffer = bufnr })
         local opts = { buffer = bufnr, remap = false }
         local additional_opts = { buffer = bufnr, remap = false, silent = true }
-        auto_format(client)
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
         vim.keymap.set('n', 'gt', function() vim.lsp.buf.type_definition() end, opts)
@@ -91,7 +91,28 @@ return {
         }
       })
 
+      lsp.format_on_save({
+          format_opts = {
+          async = true,
+          timeout_ms = 10000,
+        },
+        servers = {
+          ['null-ls'] = { 'javascript', 'typescript', 'lua' },
+        }
+      })
+
       lsp.setup()
-    end,
-  },
+
+      -- null-ls config
+          local null_ls = require('null-ls')
+
+      null_ls.setup({
+        sources = {
+          -- replace these with the tools you have installed
+          null_ls.builtins.formatting.prettierd,
+          null_ls.builtins.formatting.stylua,
+        }
+      })
+		end,
+	},
 }
