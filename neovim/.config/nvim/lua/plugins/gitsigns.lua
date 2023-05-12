aa6f0fc3d4b62c72ad76c56cf6d39544e82f3c14
Jared B <jared.j.barnak@gmail.com>
2023-05-12 12:22:45 -0400

    neovim: fixed gitsigns hacky solution

--- neovim/.config/nvim/lua/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,35 +1,31 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  dependencies = {
-    "j-barnak/nvim-window",
-  },
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    signs = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "▎" },
-      untracked = { text = "▎" },
-    },
-    on_attach = function(buffer)
-      local gs = package.loaded.gitsigns
+	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		signs = {
+			add = { text = "▎" },
+			change = { text = "▎" },
+			delete = { text = "" },
+			topdelete = { text = "" },
+			changedelete = { text = "▎" },
+			untracked = { text = "▎" },
+		},
+		on_attach = function(buffer)
+			local gs = package.loaded.gitsigns
 
-      local function map(mode, l, r, desc)
-        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-      end
+			local function map(mode, l, r, desc)
+				vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+			end
 
       -- stylua: ignore start
       map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame Line")
       map("n", "<leader>hd", gs.diffthis, "Diff This")
-      --  FIXME: Awful hack on how to exit windows
       map("n", "<leader>hD",
         function()
-          require('nvim-window').pick()
+          vim.cmd('wincmd p')
           vim.cmd(':q')
         end
         , "Go back to original window")
-    end,
-  },
+		end,
+	},
 }
