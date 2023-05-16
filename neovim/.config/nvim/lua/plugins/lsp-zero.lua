11f6f806d0672b6fa1f6f0027c8c3768874771a7
Jared B <jared.j.barnak@gmail.com>
2023-05-15 23:40:46 -0400

    Stable

--- neovim/.config/nvim/lua/plugins/lsp-zero.lua
+++ neovim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -70,29 +70,30 @@ return {
         lsp.default_keymaps({ buffer = bufnr })
         local opts = { buffer = bufnr, remap = false }
         local additional_opts = { buffer = bufnr, remap = false, silent = true }
-        vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
-        vim.keymap.set('n', 'gt', function() vim.lsp.buf.type_definition() end, opts)
-        vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set('n', '<leader>rr', function() require('telescope.builtin').lsp_references() end, additional_opts)
-        vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+        vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>rr", function() require("telescope.builtin").lsp_references() end, additional_opts)
+        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
       end)
+      -- stylua: ignore end
 
       -- Configure Language Servers Here
-      require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+      require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
-      require('lspconfig').clangd.setup({
+      require("lspconfig").clangd.setup({
         cmd = {
-          'clangd',
-          '--background-index',
-          '--clang-tidy'
-        }
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+        },
       })
 
       lsp.setup()
@@ -103,17 +104,17 @@ return {
           timeout_ms = 10000,
         },
         servers = {
-          ['null-ls'] = { 'lua', 'javascript', 'typescript' },
-          ['clangd']  = { 'cpp', 'c' },
-          ['lua_ls']  = { 'lua' }
-        }
+          ["null-ls"] = { "lua", "javascript", "typescript" },
+          ["clangd"] = { "cpp", "c" },
+        },
       })
 
-      local null_ls = require('null-ls')
+      local null_ls = require("null-ls")
 
       null_ls.setup({
         sources = {
           null_ls.builtins.formatting.prettierd,
+          null_ls.builtins.formatting.stylua,
         },
       })
     end,
