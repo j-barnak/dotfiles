9a0d8f7ebacfc7d7af4f8d246a1b6505575fea74
Jared B <jared.j.barnak@gmail.com>
2023-05-16 13:55:20 -0400

    Added nice way to use vim marks with <leader>;

--- neovim/.config/nvim/lua/config/remap.lua
+++ neovim/.config/nvim/lua/config/remap.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "q", "<Nop>", { noremap = true, silent = true }) -- This disables macros
+vim.keymap.set("n", "<leader>d", ":delmarks!<cr>", { noremap = true, silent = true }) -- This disables macros
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
