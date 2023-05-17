d80c0aba88876f35b064ebe224072bb229a92470
Jared B <jared.j.barnak@gmail.com>
2023-05-17 15:30:05 -0400

    Stable

--- neovim/.config/nvim/lua/config/remap.lua
+++ neovim/.config/nvim/lua/config/remap.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "q", "<Nop>", { noremap = true, silent = true }) -- This disables macros
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set("n", "<CR>", "<CR><Cmd>cclose<CR>", { buffer = true, silent = true })
+vim.keymap.set("n", "<CR>", "<CR><Cmd>cclose<CR>", { buffer = false, noremap = false, silent = true })
