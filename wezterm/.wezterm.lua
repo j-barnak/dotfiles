b8edc3a39cd6cb70b8fcd963ac5b458a63a92dc8
Jared B <jared.j.barnak@gmail.com>
2023-05-19 12:25:06 -0400

    Wezterm: Cursor

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,7 +1,7 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
-wezterm.on('update-right-status', function(window, pane)
+wezterm.on("update-right-status", function(window, pane)
   window:set_right_status(window:active_workspace())
 end)
 
@@ -13,57 +13,60 @@ end
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 -- config.font = wezterm.font("Berkeley Mono", { weight = "Regular", stretch = "Normal", style = "Normal" })
-config.font = wezterm.font("FiraCode Nerd Font Mono", {weight="Regular", stretch="Normal", style="Normal"})
+-- config.font = wezterm.font("FiraCode Nerd Font Mono", { weight = "Regular", stretch = "Normal", style = "Normal" })
+config.font = wezterm.font("JetBrains Mono")
 
 config.colors = {
   cursor_bg = "white",
   cursor_fg = "black",
 }
 
-config.color_scheme = 'Monokai Pro (Gogh)'
+-- config.force_reverse_video_cursor = true
+
+config.color_scheme = "Monokai Pro (Gogh)"
 
 config.window_padding = {
-  left = '0cell',
-  right = '0cell',
-  top = '0cell',
-  bottom = '0cell',
+  left = "0cell",
+  right = "0cell",
+  top = "0cell",
+  bottom = "0cell",
 }
 
 config.keys = {
   -- Switch to the default workspace
   {
-    key = ' ',
-    mods = 'SHIFT|CTRL',
-    action = act.SwitchToWorkspace {
-      name = 'default',
-    },
+    key = " ",
+    mods = "SHIFT|CTRL",
+    action = act.SwitchToWorkspace({
+      name = "default",
+    }),
   },
   -- Create a new workspace with a random name and switch to it
   {
-    key = 'n',
-    mods = 'SHIFT|CTRL',
-    action = act.SwitchToWorkspace
+    key = "n",
+    mods = "SHIFT|CTRL",
+    action = act.SwitchToWorkspace,
   },
   -- Show the launcher in fuzzy selection mode and have it list all workspaces
   -- and allow activating one.
   {
-    key = 'f',
-    mods = 'SHIFT|CTRL',
-    action = act.ShowLauncherArgs {
-      flags = 'FUZZY|WORKSPACES',
-    },
+    key = "f",
+    mods = "SHIFT|CTRL",
+    action = act.ShowLauncherArgs({
+      flags = "FUZZY|WORKSPACES",
+    }),
   },
   {
-    key = 'j',
-    mods = 'SHIFT|CTRL',
-    action = act.SwitchWorkspaceRelative(1)
+    key = "j",
+    mods = "SHIFT|CTRL",
+    action = act.SwitchWorkspaceRelative(1),
   },
   {
-    key = 'k',
-    mods = 'SHIFT|CTRL',
-    action = act.SwitchWorkspaceRelative(-1)
+    key = "k",
+    mods = "SHIFT|CTRL",
+    action = act.SwitchWorkspaceRelative(-1),
   },
 }
 
