747a91f63e851e7e1b6ef0a288e3ec122b7d4617
Jared B <jared.j.barnak@gmail.com>
2023-05-20 11:12:28 -0400

    Nvim: Saner lsp

--- neovim/.config/nvim/lua/plugins/lsp-zero.lua
+++ neovim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -87,9 +87,9 @@ return {
         lsp.default_keymaps({ buffer = bufnr })
         local opts = { buffer = bufnr, remap = false }
         local additional_opts = { buffer = bufnr, remap = false, silent = true }
-        -- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        -- vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-        -- vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+        vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
