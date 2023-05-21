e1ad96a0778d16b5d465072979f5dbed5ebbf23d
Jared B <jared.j.barnak@gmail.com>
2023-05-20 23:25:18 -0400

    Substitute added

--- neovim/.config/nvim/lua/plugins/substitute.lua
+++ neovim/.config/nvim/lua/plugins/substitute.lua
@@ -1,10 +1,14 @@
---  TODO: Learn how to use substitute to its fullest extent
 return {
   "gbprod/substitute.nvim",
   event = "VeryLazy",
   config = function()
     require("substitute").setup()
-    local sub = require("substitute.exchange")
-    vim.keymap.set("x", "X", sub.visual, { noremap = true })
+    vim.keymap.set("n", "s", require("substitute").operator, { noremap = true })
+    vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
+    vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
+    vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
+    vim.keymap.set("n", "<leader>S", require("substitute.range").operator, { noremap = true })
+    vim.keymap.set("x", "<leader>S", require("substitute.range").visual, { noremap = true })
+    vim.keymap.set("x", "X", require("substitute.exchange").visual, { noremap = true })
   end,
 }
