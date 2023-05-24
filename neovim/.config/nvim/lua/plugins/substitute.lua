6ebbbbdee8173541415ca81f57b1bf7b020b263e
Jared B <jared.j.barnak@gmail.com>
2023-05-23 20:52:30 -0400

    Removed unnecessary mapping

--- neovim/.config/nvim/lua/plugins/substitute.lua
+++ neovim/.config/nvim/lua/plugins/substitute.lua
@@ -7,8 +7,6 @@ return {
     vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
     vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
     vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
-    vim.keymap.set("n", "<leader>S", require("substitute.range").operator, { noremap = true })
-    vim.keymap.set("x", "<leader>S", require("substitute.range").visual, { noremap = true })
     vim.keymap.set("x", "X", require("substitute.exchange").visual, { noremap = true })
   end,
 }
