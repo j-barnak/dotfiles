caff1108d9869ee8960f47224c5086caa7463e87
Jared B <jared.j.barnak@gmail.com>
2023-05-31 10:51:43 -0400

    Stable

--- neovim/.config/nvim/lua/plugins/lsp-zero.lua
+++ neovim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -35,6 +35,7 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false })
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
