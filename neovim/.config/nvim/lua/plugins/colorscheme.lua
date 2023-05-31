7ae2fb3a0221ffc046c71e7b6311b3163c2cf544
Jared B <jared.j.barnak@gmail.com>
2023-05-31 19:05:45 -0400

    New Colorscheme

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,25 @@
 return {
-  "sainnhe/sonokai",
+  "loctvl842/monokai-pro.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.g.sonokai_disable_italic_comment = 0
-    vim.g.sonokai_lightline_disable_bold = 1
-    vim.g.sonokai_better_performance = 1
-    vim.cmd.colorscheme("sonokai")
+  opts = {
+    styles = {
+      comment = { italic = true },
+      keyword = { italic = false }, -- any other keyword
+      type = { italic = false }, -- (preferred) int, long, char, etc
+      storageclass = { italic = false }, -- static, register, volatile, etc
+      structure = { italic = false }, -- struct, union, enum, etc
+      parameter = { italic = false }, -- parameter pass in function
+      annotation = { italic = false },
+      tag_attribute = { italic = false }, -- attribute of tag in reactjs
+    },
+    filter = "machine",
+  },
+  -- TODO: Fix 
+  config = function(_, opts)
+    require("monokai-pro").setup(opts)
+    vim.cmd([[colorscheme monokai-pro]])
     vim.cmd("highlight! link Todo Comment") -- Must be after colorscheme
-    vim.cmd([[hi NvimTreeWinSeparator guifg=#2c2e34 guibg=NONE]]) -- Must be after colorscheme
+    vim.cmd([[hi NvimTreeWinSeparator guifg=#273136 guibg=NONE]]) -- Must be after colorscheme
   end,
 }
