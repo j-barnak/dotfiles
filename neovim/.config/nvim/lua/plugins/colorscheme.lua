728af5c82f86fb3a36c60a3662081cbe72ba15a5
Jared B <jared.j.barnak@gmail.com>
2023-05-31 22:04:57 -0400

    Stable

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,20 +5,20 @@ return {
   opts = {
     styles = {
       comment = { italic = true },
-      keyword = { italic = false }, -- any other keyword
-      type = { italic = false }, -- (preferred) int, long, char, etc
-      storageclass = { italic = false }, -- static, register, volatile, etc
-      structure = { italic = false }, -- struct, union, enum, etc
-      parameter = { italic = false }, -- parameter pass in function
+      keyword = { italic = false },
+      type = { italic = false },
+      storageclass = { italic = false },
+      structure = { italic = false },
+      parameter = { italic = false },
       annotation = { italic = false },
-      tag_attribute = { italic = false }, -- attribute of tag in reactjs
+      tag_attribute = { italic = false },
     },
     filter = "machine",
   },
   config = function(_, opts)
     require("monokai-pro").setup(opts)
     vim.cmd([[colorscheme monokai-pro]])
-    vim.cmd("highlight! link Todo Comment") -- Must be after colorscheme
-    vim.cmd([[hi NvimTreeWinSeparator guifg=#273136 guibg=NONE]]) -- Must be after colorscheme
+    vim.cmd("highlight! link Todo Comment")
+    vim.cmd([[hi NvimTreeWinSeparator guifg=#273136 guibg=NONE]])
   end,
 }
