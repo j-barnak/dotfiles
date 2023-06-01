927bf9ba3d35723e8d1ce1c05161d3f87c3e303b
Jared B <jared.j.barnak@gmail.com>
2023-05-31 20:41:06 -0400

    Stable

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -13,24 +13,25 @@ return {
       annotation = { italic = false },
       tag_attribute = { italic = false }, -- attribute of tag in reactjs
     },
+    -- TODO: Match sonokai
     filter = "machine",
-    override = function(c)
-      return {
-        Normal = { bg = "#2C2E34" },
-        SignColumn = { bg = "#2C2E34" },
-        CursorColumn = { bg = "#2C2E34" },
-        ColorColumn = { bg = "#2C2E34" },
-        LineNr = {
-          bg = "#2C2E34",
-          fg = c.editorLineNumber.foreground,
-        },
-        CursorLineNr = {
-          bg = "#333846",
-          fg = c.editorLineNumber.activeForeground,
-          bold = true,
-        },
-      }
-    end,
+    -- override = function(c)
+    --   return {
+    --     Normal = { bg = "#2C2E34" },
+    --     SignColumn = { bg = "#2C2E34" },
+    --     CursorColumn = { bg = "#2C2E34" },
+    --     ColorColumn = { bg = "#2C2E34" },
+    --     LineNr = {
+    --       bg = "#2C2E34",
+    --       fg = c.editorLineNumber.foreground,
+    --     },
+    --     CursorLineNr = {
+    --       bg = "#333846",
+    --       fg = c.editorLineNumber.activeForeground,
+    --       bold = true,
+    --     },
+    --   }
+    -- end,
   },
   -- TODO: Fix
   config = function(_, opts)
@@ -40,3 +41,16 @@ return {
     vim.cmd([[hi NvimTreeWinSeparator guifg=#273136 guibg=NONE]]) -- Must be after colorscheme
   end,
 }
+-- return {
+--   "sainnhe/sonokai",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.g.sonokai_disable_italic_comment = 0
+--     vim.g.sonokai_lightline_disable_bold = 1
+--     vim.g.sonokai_better_performance = 1
+--     vim.cmd.colorscheme("sonokai")
+--     vim.cmd("highlight! link Todo Comment") -- Must be after colorscheme
+--     vim.cmd([[hi NvimTreeWinSeparator guifg=#2c2e34 guibg=NONE]])
+--   end,
+-- }
