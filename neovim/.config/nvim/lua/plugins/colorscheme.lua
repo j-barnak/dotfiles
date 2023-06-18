7429ee8d202cf0b6ae74dc8a942d388e3f05a2b2
Jared Barnak <jared.j.barnak@gmail.com>
2023-06-18 10:46:54 -0400

    Stable

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,26 @@
 return {
-  "sainnhe/sonokai",
+  "catppuccin/nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.g.sonokai_disable_italic_comment = 0
-    vim.g.sonokai_lightline_disable_bold = 1
-    vim.g.sonokai_better_performance = 1
-    vim.cmd.colorscheme("sonokai")
+  opts = {
+    flavour = "frappe", -- latte, frappe, macchiato, mocha
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
     vim.cmd("highlight! link Todo Comment") -- Must be after colorscheme
-    vim.cmd([[hi NvimTreeWinSeparator guifg=#2c2e34 guibg=NONE]])
   end,
 }
+-- return {
+--   "sainnhe/sonokai",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.g.sonokai_disable_italic_comment = 0
+--     vim.g.sonokai_lightline_disable_bold = 1
+--     vim.g.sonokai_better_performance = 1
+--     vim.cmd.colorscheme("sonokai")
+--     vim.cmd("highlight! link Todo Comment") -- Must be after colorscheme
+--     vim.cmd([[hi NvimTreeWinSeparator guifg=#2c2e34 guibg=NONE]])
+--   end,
+-- }
---
+++ neovim/.config/nvim/lua/plugins/searchbox.lua
@@ -0,0 +1,10 @@
+return {
+  "VonHeikemen/searchbox.nvim",
+  dependencies = {
+    { "MunifTanjim/nui.nvim" },
+  },
+  keys = {
+    { "<leader>o", "<cmd>SearchBoxMatchAll<cr>", desc = "Search box" },
+  },
+  opts = {},
+}
