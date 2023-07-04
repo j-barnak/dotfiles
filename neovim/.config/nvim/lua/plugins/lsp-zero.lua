bc12f062bdcd83d6f1bebc803748b85f4f1fec2a
Jared B <jared.j.barnak@gmail.com>
2023-07-03 23:19:18 -0400

    Stable

--- neovim/.config/nvim/lua/plugins/lsp-zero.lua
+++ neovim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -156,6 +156,18 @@ return {
         },
       })
 
+      -- TODO: Enable when nvim 10.0 comes out
+      -- vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+      --   callback = function()
+      --     vim.lsp.buf.inlay_hint(0, true)
+      --   end,
+      -- })
+      -- vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+      --   callback = function()
+      --     vim.lsp.buf.inlay_hint(0, false)
+      --   end,
+      -- })
+
       lsp.setup()
 
       lsp.format_on_save({
