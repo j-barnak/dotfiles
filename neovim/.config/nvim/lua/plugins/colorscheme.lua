52f3425aeca94f472abca6febcb8cd4ac41379dd
Jared B <jared.j.barnak@gmail.com>
2023-07-28 01:02:55 -0400

    Stable

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -11,16 +11,3 @@ return {
     vim.cmd("highlight! link Todo Comment") -- Must be after colorscheme
   end,
 }
--- return {
---   "sainnhe/sonokai",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     vim.g.sonokai_disable_italic_comment = 0
---     vim.g.sonokai_lightline_disable_bold = 1
---     vim.g.sonokai_better_performance = 1
---     vim.cmd.colorscheme("sonokai")
---     vim.cmd("highlight! link Todo Comment") -- Must be after colorscheme
---     vim.cmd([[hi NvimTreeWinSeparator guifg=#2c2e34 guibg=NONE]])
---   end,
--- }
