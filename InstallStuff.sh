3c3a5a1565497f1ac53d97a853d52250498c83d2
jared <jared.j.barnak@gmail.com>
2023-08-06 12:24:45 -0400

    fronk

---
+++ InstallStuff.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+sudo apt install -y build-essential ripgrep curl fd-find exa fzf zsh stow ninja-build gettext unzip curl libssl-dev && \
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash && \
+bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
