479fb27f9323930055c39c9ba9f752f09e8c0a17
jared <jared.j.barnak@gmail.com>
2023-08-07 00:37:00 -0400

    frank

--- home/.config/nvim/lua/plugins/formatter.lua
+++ home/.config/nvim/lua/plugins/formatter.lua
@@ -33,5 +33,11 @@ return {
         },
       },
     })
+
+    vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      group = "FormatAutogroup",
+      command = "FormatWrite",
+    })
   end,
 }
