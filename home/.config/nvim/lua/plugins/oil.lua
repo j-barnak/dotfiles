33b05d1ad3d79142e64f5d35eb63b64f08d931ae
jared <jared.j.barnak@gmail.com>
2023-08-06 23:36:35 -0400

    Frank

--- home/.config/nvim/lua/plugins/oil.lua
+++ home/.config/nvim/lua/plugins/oil.lua
@@ -22,4 +22,8 @@ return {
     use_default_keymaps = true,
   },
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
 }
