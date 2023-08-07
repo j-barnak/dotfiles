64cc3bde25bda8c762f976fdf1b2847635e1e72f
jared <jared.j.barnak@gmail.com>
2023-08-06 23:40:19 -0400

    Frank

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -23,6 +23,7 @@ return {
         end,
       })
 
+      -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
       lspconfig.emmet_language_server.setup({})
       lspconfig.tsserver.setup({})
 
