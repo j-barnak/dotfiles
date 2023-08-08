34f4dd02675ec941760cd12f74121f9096c62ddd
jared <jared.j.barnak@gmail.com>
2023-08-08 02:33:06 -0400

    Frank

--- home/.config/nvim/lua/plugins/noice.lua
+++ home/.config/nvim/lua/plugins/noice.lua
@@ -1,24 +1,3 @@
--- return {
---   "folke/noice.nvim",
---   event = "VeryLazy",
---   dependencies = {
---     "MunifTanjim/nui.nvim",
---   },
---   opts = {
---     lsp = {
---       override = {
---         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
---         ["vim.lsp.util.stylize_markdown"] = true,
---         ["cmp.entry.get_documentation"] = true,
---       },
---     },
---   },
---   config = function(_, opts)
---     require("noice").setup(opts)
---   end,
--- }
---
--- Nice notifications, command line UI, and more.
 return {
   {
     "folke/noice.nvim",
@@ -58,9 +37,7 @@ return {
 
       require("noice").setup({
         presets = {
-          -- Have borders around hover and signature help.
           lsp_doc_border = true,
-          -- command_palette = true,
           long_message_to_split = true,
         },
         lsp = {
@@ -69,17 +46,14 @@ return {
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true,
           },
-          -- I use a keymap for opening this when needed.
           signature = {
             auto_open = { enabled = false },
           },
         },
         status = {
-          -- Statusline component for LSP progress notifications.
           lsp_progress = { event = "lsp", kind = "progress" },
         },
         routes = {
-          -- Ignore the typical vim change messages.
           {
             filter = {
               event = "msg_show",
@@ -93,7 +67,6 @@ return {
             },
             opts = { skip = true },
           },
-          -- Don't show these in the default view, use Lualine instead.
           {
             filter = {
               event = "lsp",
@@ -103,11 +76,9 @@ return {
           },
         },
         cmdline = {
-          -- Use the default formats, but don't conceal the prefixes.
           format = cmdline_formats,
         },
         format = {
-          -- Looks better for Lualine.
           progress = { align = "left" },
         },
       })
