16cf3e4a3d2c87c232d1402120a8aabc2c98e8b7
jared <jared.j.barnak@gmail.com>
2023-08-10 15:59:39 -0400

    Frank

--- home/.config/nvim/lua/plugins/illuminate.lua
+++ home/.config/nvim/lua/plugins/illuminate.lua
@@ -8,22 +8,4 @@ return {
       providers = { "lsp" },
     },
   },
-  config = function(_, opts)
-    local illuminate = require("illuminate")
-
-    illuminate.configure(opts)
-
-    local illuminate_group = vim.api.nvim_create_augroup("IlluminateBufUpdate", { clear = true })
-    vim.api.nvim_create_autocmd("BufEnter", {
-      group = illuminate_group,
-      callback = illuminate.resume_buf,
-    })
-    vim.api.nvim_create_autocmd("BufLeave", {
-      group = illuminate_group,
-      callback = illuminate.pause_buf,
-    })
-
-    vim.keymap.del({ "o", "x" }, "<M-i>")
-    vim.api.nvim_set_hl(0, "IlluminatedWordText", { underline = false })
-  end,
 }
