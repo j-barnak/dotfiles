2e6e4249501561f1dc4982f907c92c6f982458e2
jared <jared.j.barnak@gmail.com>
2023-08-12 02:27:49 -0400

    Working on TODOs in config

--- home/.config/nvim/lua/plugins/todo.lua
+++ home/.config/nvim/lua/plugins/todo.lua
@@ -1,12 +1,13 @@
--- TODO: - Remove icons
+-- TODO: - Remove icons for every thing
 --       - Add to telescope
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "View TODOs and similar" },
+  },
   opts = {
-    keywords = {
-      TODO = { icon = "" },
-    },
+    signs = false, -- show icons in the signs column
     highlight = {
       keyword = "fg",
     },
