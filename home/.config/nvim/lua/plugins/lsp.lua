48f263271ea4b7ac0555f1fcd04d509e5f8495d2
jared <jared.j.barnak@gmail.com>
2023-08-12 02:02:17 -0400

    LSP lua_ls new settings

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -47,20 +47,26 @@ return {
       lspconfig.lua_ls.setup({
         settings = {
           Lua = {
-            runtime = {
-              version = "LuaJIT",
-            },
             diagnostics = {
               globals = { "vim" },
             },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-            telemetry = {
-              enable = false,
-            },
           },
         },
+        on_init = function(client)
+          local path = client.workspace_folders[1].name
+          if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+            client.config.settings = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+              runtime = {
+                version = "LuaJIT",
+              },
+              workspace = {
+                library = { vim.env.VIMRUNTIME },
+              },
+            })
+            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+          end
+          return true
+        end,
       })
     end,
   },
