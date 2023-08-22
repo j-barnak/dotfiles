6026bf95407d5d2884a0c90f8d78d7aabf06c9c4
jared <jared.j.barnak@gmail.com>
2023-08-22 16:09:06 -0400

    Frank

--- home/.config/nvim/lua/plugins/flash.lua
+++ home/.config/nvim/lua/plugins/flash.lua
@@ -5,6 +5,23 @@ return {
     search = {
       mode = "fuzzy",
     },
+    modes = {
+      char = {
+        keys = {},
+      },
+    },
+    prompt = {
+      enabled = true,
+      prefix = { { "Flash: ", "FlashPromptIcon" } },
+      win_config = {
+        relative = "editor",
+        width = 1, -- when <=1 it's a percentage of the editor width
+        height = 1,
+        row = -1, -- when negative it's an offset from the bottom
+        col = 0, -- when negative it's an offset from the right
+        zindex = 1000,
+      },
+    },
   },
   keys = {
     {
