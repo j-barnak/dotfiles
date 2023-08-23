f93b43a43b69b1be6ddceb8a650e726b230b3ccf
jared <jared.j.barnak@gmail.com>
2023-08-23 15:05:54 -0400

    Okay, I _think_ I'm done config changing.

--- home/.config/nvim/lua/plugins/flash.lua
+++ home/.config/nvim/lua/plugins/flash.lua
@@ -9,10 +9,32 @@ return {
     modes = {
       char = {
         jump_labels = true,
+        keys = { "t", "T", ";", "," },
       },
+    },
+    prompt = {
+      prefix = { { "Flash: ", "FlashPromptIcon" } },
     },
   },
   keys = {
+    {
+      "f",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").jump({
+          search = { forward = true, wrap = false, multi_window = false },
+        })
+      end,
+    },
+    {
+      "F",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").jump({
+          search = { forward = false, wrap = false, multi_window = false },
+        })
+      end,
+    },
     {
       "s",
       mode = { "n", "o", "x" },
