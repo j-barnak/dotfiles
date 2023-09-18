3f493fa9ab727bbba75c0e12b4fd020a012ec74a
jared <jared.j.barnak@gmail.com>
2023-09-18 11:09:35 -0400

    Fixed text objects and commenting

--- home/.config/nvim/lua/plugins/comment.lua
+++ home/.config/nvim/lua/plugins/comment.lua
@@ -2,5 +2,6 @@ return {
   "numToStr/Comment.nvim",
   opts = {
     toggler = { line = "<leader>cc", block = "<leader>bc" },
+    opleader = { line = "<leader>c", block = "<leader>b" },
   },
 }
--- home/.config/nvim/lua/plugins/mini-ai.lua
+++ home/.config/nvim/lua/plugins/mini-ai.lua
@@ -1,25 +1,26 @@
 return {
-  {
-    "echasnovski/mini.ai",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
-    opts = function()
-      local miniai = require("mini.ai")
-
-      return {
-        n_lines = 300,
-        custom_textobjects = {
-          f = miniai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-        },
-        silent = true,
-        search_method = "cover",
-        mappings = {
-          around_next = "",
-          inside_next = "",
-          around_last = "",
-          inside_last = "",
-        },
-      }
-    end,
+  "echasnovski/mini.ai",
+  keys = {
+    { "a", mode = { "x", "o" } },
+    { "i", mode = { "x", "o" } },
   },
+  event = "VeryLazy",
+  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+  opts = function()
+    local ai = require("mini.ai")
+    return {
+      n_lines = 500,
+      custom_textobjects = {
+        o = ai.gen_spec.treesitter({
+          a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+          i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+        }, {}),
+        f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+        c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+      },
+    }
+  end,
+  config = function(_, opts)
+    require("mini.ai").setup(opts)
+  end,
 }
