798faef0bb8ad35e40c72df93a315334def7da16
jared <jared.j.barnak@gmail.com>
2023-10-02 07:50:40 -0400

    Better harpoon

--- home/.config/nvim/lua/plugins/harpoon.lua
+++ home/.config/nvim/lua/plugins/harpoon.lua
@@ -1,15 +1,21 @@
 return {
-  "ThePrimeagen/harpoon",
+  "MeanderingProgrammer/harpoon-core.nvim",
   event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
   keys = {
     -- stylua: ignore start
-    { "<leader>a", function () require("harpoon.mark").add_file() end },
-    { "<leader>e", function () require("harpoon.ui").toggle_quick_menu() end },
-    { "<leader>1", function () require("harpoon.ui").nav_file(1)  end },
-    { "<leader>2", function () require("harpoon.ui").nav_file(2)  end },
-    { "<leader>3", function () require("harpoon.ui").nav_file(3)  end },
-    { "<leader>4", function () require("harpoon.ui").nav_file(4)  end },
+    { "<leader>a", function () require("harpoon-core.mark").add_file() end },
+    { "<leader>e", function () require("harpoon-core.ui").toggle_quick_menu() end },
+    { "<leader>1", function () require("harpoon-core.ui").nav_file(1)  end },
+    { "<leader>2", function () require("harpoon-core.ui").nav_file(2)  end },
+    { "<leader>3", function () require("harpoon-core.ui").nav_file(3)  end },
+    { "<leader>4", function () require("harpoon-core.ui").nav_file(4)  end },
     -- stylua: ignore end
   },
   opts = {},
+  config = function(_, opts)
+    require("harpoon-core").setup(opts)
+  end,
 }
