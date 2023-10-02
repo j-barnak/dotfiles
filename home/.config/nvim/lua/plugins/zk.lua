99d863ff3d92998d1a47eccbdc7e85d018370d19
jared <jared.j.barnak@gmail.com>
2023-10-02 08:44:06 -0400

    Better ZK

--- home/.config/nvim/lua/plugins/zk.lua
+++ home/.config/nvim/lua/plugins/zk.lua
@@ -1,8 +1,7 @@
 return {
   "mickael-menu/zk-nvim",
-  config = function()
-    require("zk").setup({
-      -- See Setup section below
-    })
+  opts = {},
+  config = function(_, opts)
+    require("zk").setup(opts)
   end,
 }
