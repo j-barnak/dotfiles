e130a0045d06f0c646a621e4eecfc059c3311655
j-barnak <96548573+j-barnak@users.noreply.github.com>
2023-10-14 14:02:06 -0400

    nvim-pair replaces mini-pairs

--- home/.config/nvim/lua/plugins/pair.lua
+++ home/.config/nvim/lua/plugins/pair.lua
@@ -1,16 +1,30 @@
 return {
-	"echasnovski/mini.pairs",
-	dependencies = {
-		"hrsh7th/nvim-cmp",
-	},
-	event = "VeryLazy",
-	config = function()
-		require("mini.pairs").setup()
-		require("mini.pairs").map(
-			"i",
-			"<",
-			{ action = "open", pair = "<>", neigh_pattern = "\r.", register = { cr = false } }
-		)
-		require("mini.pairs").map("i", ">", { action = "close", pair = "<>", register = { cr = false } })
-	end,
+  {
+    "windwp/nvim-autopairs",
+    dependencies = {
+      { "hrsh7th/nvim-cmp" },
+    },
+    event = "InsertEnter",
+    opts = {},
+    config = function(_, opts)
+      local npairs = require("nvim-autopairs")
+      local rule = require("nvim-autopairs.rule")
+      local cond = require("nvim-autopairs.conds")
+
+      npairs.setup(opts)
+
+      local is_template = require("util.pair").is_template
+      local semicolon = require("util.pair").struct_class_semicolon
+
+      npairs.add_rules({
+        rule("<", ">"):with_pair(cond.none()):with_move(cond.done()):use_key(">"),
+      })
+
+      npairs.add_rules({
+        rule("{", "};", { "cpp", "c" }):with_pair(semicolon),
+      })
+
+      vim.keymap.set("i", "<", is_template)
+    end,
+  },
 }
