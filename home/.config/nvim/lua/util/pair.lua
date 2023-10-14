ac07240cc8624708c06d0d9e1ac0f8a8deb23a59
j-barnak <96548573+j-barnak@users.noreply.github.com>
2023-10-14 14:00:40 -0400

    Util functions for nvim-pair
    Usage
    
    ```return {
      {
        "windwp/nvim-autopairs",
        dependencies = {
          { "hrsh7th/nvim-cmp" },
        },
        event = "InsertEnter",
        opts = {}, -- this is equalent to setup({}) function
        config = function(_, opts)
          local npairs = require("nvim-autopairs")
          local rule = require("nvim-autopairs.rule")
          local cond = require("nvim-autopairs.conds")
    
          npairs.setup(opts)
    
          local is_template = require("util.pair").is_template
          local semicolon = require("util.pair").struct_class_semicolon
    
          npairs.add_rules({
            rule("<", ">"):with_pair(cond.none()):with_move(cond.done()):use_key(">"), -- a random key so it doesn't map <
          })
    
          npairs.add_rules({
            rule("{", "};", { "cpp", "c" }):with_pair(semicolon),
          })
    
          vim.keymap.set("i", "<", is_template)
        end,
      },
    }
    ```

---
+++ home/.config/nvim/lua/util/pair.lua
@@ -0,0 +1,61 @@
+local M = {}
+
+function M.is_template()
+  local unpack = table.unpack or unpack
+
+  local line = vim.api.nvim_get_current_line()
+  local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+  if not (vim.o.filetype == "cpp" or vim.o.filetype == "c") then
+    line = line:sub(1, c) .. "<" .. line:sub(c + 1)
+    vim.api.nvim_set_current_line(line)
+    vim.api.nvim_win_set_cursor(0, { r, c + 1 })
+    return
+  end
+
+  if vim.fn.match({ line }, "template") == 0 then
+    -- c - 1 = 2 chars before the cursor
+    line = line:sub(1, c) .. "<>" .. line:sub(c + 1)
+    vim.api.nvim_set_current_line(line)
+    vim.api.nvim_win_set_cursor(0, { r, c + 1 })
+    return
+  end
+
+  if vim.fn.match({ line }, "#include") == 0 then
+    line = line:sub(1, c) .. "<>" .. line:sub(c + 1)
+    if line:sub(c, c) ~= " " then
+      line = line:sub(1, c) .. " " .. line:sub(c + 1)
+      c = c + 1
+    end
+    vim.api.nvim_set_current_line(line)
+    vim.api.nvim_win_set_cursor(0, { r, c + 1 })
+    return
+  end
+
+  line = line:sub(1, c) .. "<" .. line:sub(c + 1)
+  vim.api.nvim_set_current_line(line)
+  vim.api.nvim_win_set_cursor(0, { r, c + 1 })
+  vim.cmd("redraw") -- redraw to add the first <
+
+  local old_handler = vim.lsp.handlers["textDocument/signatureHelp"]
+  vim.lsp.handlers["textDocument/signatureHelp"] = function(_, info)
+    if info and info.signatures and info.signatures[1] and info.signatures[1].label then
+      local functionsig = info.signatures[1].label
+      if vim.fn.match({ functionsig }, "^\\w\\+<") == 0 then
+        vim.api.nvim_set_current_line(line:sub(0, c + 1) .. ">" .. line:sub(c + 2))
+      end
+    end
+  end
+  vim.lsp.buf.signature_help()
+  vim.lsp.handlers["textDocument/signatureHelp"] = old_handler
+end
+
+function M.struct_class_semicolon()
+  local line = vim.api.nvim_get_current_line()
+  local pattern = "^.*(.*)"
+
+  if vim.fn.match({ line }, pattern) == 0 then
+    return false
+  end
+end
+
+return M
