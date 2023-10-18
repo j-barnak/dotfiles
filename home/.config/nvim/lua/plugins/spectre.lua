65789a600aa7a6dd00123f6247d0298ea40259be
jared <jared.j.barnak@gmail.com>
2023-10-18 12:28:20 -0400

    Better Spectre

--- home/.config/nvim/lua/plugins/spectre.lua
+++ home/.config/nvim/lua/plugins/spectre.lua
@@ -3,13 +3,32 @@ return {
   opts = {
     mapping = {
       ["close_spectre"] = {
-        map = "<leader>Q",
+        map = "<leader>q",
         cmd = "<cmd>q!<cr>",
         desc = "Close spectre",
+      },
+      ["send_to_qf"] = {
+        map = "<leader>T",
+        cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+        desc = "send all items to quickfix",
       },
     },
   },
   keys = {
-    { "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', desc = "Toggle Spectre" },
+    {
+      "<leader>S",
+      '<cmd>lua require("spectre").toggle()<CR>',
+      desc = "Toggle Spectre",
+    },
+    {
+      "<leader>W",
+      '<esc><cmd>lua require("spectre").open_visual()<CR>',
+      mode = { "v" },
+      desc = "Toggle Spectre",
+    },
   },
 }
+
+-- vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+-- desc = "Search current word"
+-- })
