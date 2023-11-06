72f632c1163725f2f101f3a8044945ad03a676a6
jared <jared.j.barnak@gmail.com>
2023-11-05 19:15:59 -0500

    updated pairs plugin

--- home/.config/nvim/lua/plugins/pair.lua
+++ home/.config/nvim/lua/plugins/pair.lua
@@ -10,23 +10,14 @@ return {
       local npairs = require("nvim-autopairs")
       local rule = require("nvim-autopairs.rule")
       local cond = require("nvim-autopairs.conds")
-      local ts_conds = require("nvim-autopairs.ts-conds")
 
       npairs.setup(opts)
 
       local is_template = require("util.pair").is_template
       local semicolon = require("util.pair").struct_class_semicolon
-
       npairs.add_rules({
         rule("<", ">"):with_pair(cond.none()):with_move(cond.done()):use_key(">"),
-      })
-
-      npairs.add_rules({
         rule("{", "};", { "cpp", "c" }):with_pair(semicolon),
-      })
-
-      npairs.add_rules({
-        rule("{", "}", "cpp"):with_pair(ts_conds.is_ts_node({ "namespace_definition" })),
       })
 
       vim.keymap.set("i", "<", is_template)
--- home/.config/nvim/lua/util/pair.lua
+++ home/.config/nvim/lua/util/pair.lua
@@ -30,17 +30,26 @@ function M.is_template()
     vim.api.nvim_win_set_cursor(0, { r, c + 1 })
     return
   end
+  if vim.fn.match({ line:sub(0, c) }, "cast\\s*$") == 0 then
+    -- c - 1 = 2 chars before the cursor
+    line = line:sub(1, c) .. "<>" .. line:sub(c + 1)
+    vim.api.nvim_set_current_line(line)
+    vim.api.nvim_win_set_cursor(0, { r, c + 1 })
+    return
+  end
 
+  -- c is 1 before the cursor
   line = line:sub(1, c) .. "<" .. line:sub(c + 1)
   vim.api.nvim_set_current_line(line)
   vim.api.nvim_win_set_cursor(0, { r, c + 1 })
   vim.cmd("redraw") -- redraw to add the first <
-
+  -- since we added < the lsp can detect it
   local old_handler = vim.lsp.handlers["textDocument/signatureHelp"]
   vim.lsp.handlers["textDocument/signatureHelp"] = function(_, info)
     if info and info.signatures and info.signatures[1] and info.signatures[1].label then
       local functionsig = info.signatures[1].label
       if vim.fn.match({ functionsig }, "^\\w\\+<") == 0 then
+        -- c + 1 is after including the openning pair very shady code lol
         vim.api.nvim_set_current_line(line:sub(0, c + 1) .. ">" .. line:sub(c + 2))
       end
     end
@@ -51,9 +60,10 @@ end
 
 function M.struct_class_semicolon()
   local line = vim.api.nvim_get_current_line()
-  local pattern = "^.*(.*)"
+  -- match for() while() functions()
+  local pattern = "\\m^.*(.*)"
 
-  if vim.fn.match({ line }, pattern) == 0 then
+  if vim.fn.match(line, pattern) ~= -1 or vim.fn.match(line, "namespace") ~= -1 then
     return false
   end
 end
