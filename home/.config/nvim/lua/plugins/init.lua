08070b2f9bf4559f8824fb632553d53c3506321b
j-barnak <96548573+j-barnak@users.noreply.github.com>
2024-05-21 00:07:21 -0400

    Update init.lua

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -3,13 +3,6 @@ return {
     "stevearc/dressing.nvim",
   },
   {
-    "felipec/vim-sanegx",
-  },
-  {
     "tpope/vim-fugitive",
   },
-  -- TODO: When 0.10 stable releases, uncomment
-  -- {
-  --   "Bekaboo/dropbar.nvim"
-  -- },
 }
